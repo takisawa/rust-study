43e94363cf6306f5923ff7acc8ae1f0128247381
Hiroaki Takisawa <takisawa@hit-start.jp>
2022-05-30 09:08:54 +0900

    feature: connecting postgres from rust program

--- connect_postgres/src/main.rs
+++ connect_postgres/src/main.rs
@@ -1,5 +1,17 @@
 extern crate postgres;
+use postgres::{Client, Error, NoTls};
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), Error> {
+    let mut client = Client::connect(
+        "postgresql://postgres:password@127.0.0.1:5432/testdb",
+        NoTls,
+    )?;
+
+    for row in client.query("SELECT id, name FROM users", &[])? {
+        let id: i32 = row.get(0);
+        let name: String = row.get(1);
+        println!("{} {}", id, name);
+    }
+
+    Ok(())
 }
